79b0cccabb214dba497886c4844392c3b1a86c5c
salekinraju <salekinraju@gmail.com>
2026-02-21 16:26:35 +0600

    Added Description

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Polymorphism = "Poly" = "Many" "Morph" = "Shape"
+        // Objects can identify as other objects. Objects can be treated as objects of a common superclass
+
+
             Car car = new Car();
             Bike bike = new Bike();
             Boat boat = new Boat();
